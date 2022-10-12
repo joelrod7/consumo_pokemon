afe043666d712951f194af8c95b3468f68837e0e
JoelRod7 <joeldjesusr@gmail.com>
2022-10-12 17:35:13 -0500

    Cada item con estilo

--- src/componente/Detalles.jsx
+++ src/componente/Detalles.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Carta} from './Carta'
+import './Formas.css'
 
 export const Detalles = ({results}) => {
     return (
         <div className='container'>
-            <ul className='detalles'>
+            <ul className='cartas'>
                 {
                     results.map( p=>(
-                        <li>
+                        <li className='item' key={p.name}>
                             <Carta url={p.url}/>
                         </li>
                     ))
